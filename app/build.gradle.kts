c3db35dc0f5b10ec0c91c28c1915f07505864c54
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2020-11-12 00:09:33 +0300

    2 task one (#2)
    * Fixed project strucutre
    Fixed lib dependecny
    
    * Finished first task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,13 +9,24 @@ repositories {
     jcenter()
 }
 
+val lombokVersion = "1.18.14";
+
 dependencies {
 
     implementation(":airport-1.0.1")
 
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+
+    testCompileOnly("org.projectlombok:lombok:$lombokVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
 }
 
 application {
